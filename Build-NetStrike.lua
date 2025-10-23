fa57e0f906fc6d48efdeb012834cc7620c9e1fe2
Anupam Sahu - SATURN <sahuanupam7@gmail.com>
2025-10-24 00:45:40 +0100

    removed core and linked sfml

--- Build-NetStrike.lua
+++ Build-NetStrike.lua
@@ -12,7 +12,17 @@ startproject "Game"
 
 --Workspace-wide build options for MSVC
 filter "system:windows"
+filter "configurations:Debug"
 buildoptions 
+{
+    "/EHsc",
+    "/Zc:preprocessor",
+    "/Zc:__cplusplus",
+    "/D_ITERATOR_DEBUG_LEVEL=2"
+}
+
+filter "configurations:Release"
+buildoptions
 {
     "/EHsc",
     "/Zc:preprocessor",
@@ -28,5 +38,4 @@ linkoptions
 
 OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
 
-include "Projects/Core/Build-Core.lua"
 include "Projects/Game/Build-Game.lua"
--- Projects/Game/Source/Game.cpp
+++ Projects/Game/Source/Game.cpp
@@ -1,8 +1,24 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "Hello Game!" << "\n"; 
+	sf::RenderWindow Window(sf::VideoMode({1280, 720}), "!!NetStrike!!");
+
+	while (Window.isOpen())
+	{
+		const std::optional<sf::Event> Event = Window.pollEvent();
+		if (Event)
+		{
+			if (Event->is<sf::Event::Closed>())
+			{
+				Window.close();
+			}
+		}
+		
+		Window.clear();
+		Window.display();
+	}
+
 	
 	return 0;
 }
