6367c6be67419d77eb4e04c6ef54dfa17bd951c4
Anupam Sahu - SATURN <sahuanupam7@gmail.com>
2025-10-24 15:41:21 +0100

    added a logger

--- Projects/Game/Source/Main.cpp
+++ Projects/Game/Source/Main.cpp
@@ -1,9 +1,11 @@
 #include <SFML/Graphics.hpp>
 
+#include "Logger.h"
+
 int main()
 {
 	sf::RenderWindow Window(sf::VideoMode({1280, 720}), "!!NetStrike!!");
-
+	NSLOG(NS::ELogLevel::INFO, "Created a new window!");
 	while (Window.isOpen())
 	{
 		const std::optional<sf::Event> Event = Window.pollEvent();
@@ -11,6 +13,7 @@ int main()
 		{
 			if (Event->is<sf::Event::Closed>())
 			{
+				NSLOG(NS::ELogLevel::INFO, "Shutdown request received. Closing window!");
 				Window.close();
 			}
 		}
@@ -19,6 +22,6 @@ int main()
 		Window.display();
 	}
 
-	
+	NSLOG(NS::ELogLevel::WARNING, "Application exiting...");
 	return 0;
 }
