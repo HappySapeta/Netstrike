e10f13b35e75bb4a2f9d5d0cac12df03d187716c
Anupam Sahu - SATURN <sahuanupam7@gmail.com>
2025-10-28 11:15:33 +0000

    New actor class and Tank actor.

---
+++ Projects/Game/Include/Actor/Actor.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Transform.hpp>
+#include <SFML/System/Vector2.hpp>
+
+namespace NS
+{
+	struct Transform : sf::Transform
+	{
+		Transform();
+		
+		sf::Vector2f Position;
+		sf::Vector2f Rotation;
+		sf::Vector2f Scale;
+	};
+
+	class Actor
+	{
+	public:
+
+		Actor() = default;
+		virtual ~Actor() = default;
+
+	public:
+
+		virtual void Draw(sf::RenderWindow& Window) = 0;
+		virtual void Update(const float DeltaTime) = 0;
+
+	protected:
+
+		Transform Transform_;
+	};
+}
---
+++ Projects/Game/Source/Tank.cpp
@@ -0,0 +1,31 @@
+﻿#include "Tank.h"
+
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Window/Window.hpp>
+
+#include "Logger.h"
+
+NS::Tank::Tank()
+{
+	Debug_SetTexture("../../Resources/Tank.png");
+}
+
+void NS::Tank::Draw(sf::RenderWindow& Window)
+{
+	sf::Sprite Sprite(Texture_);
+	Window.draw(Sprite);
+}
+
+void NS::Tank::Update(const float DeltaTime)
+{
+}
+
+void NS::Tank::Debug_SetTexture(const char* TextureLocation)
+{
+	if (!Texture_.loadFromFile(TextureLocation))
+	{
+		NSLOG(ELogLevel::ERROR, "Failed to find texture. ", TextureLocation);
+	}
+}
