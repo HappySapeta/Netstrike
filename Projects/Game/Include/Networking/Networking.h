a3d5d4eb7b36430f63fe7980ccbf28b6d2bdfc15
Anupam Sahu - SATURN <sahuanupam7@gmail.com>
2025-11-01 19:15:34 +0000

    Networking class

---
+++ Projects/Game/Include/Networking/Networking.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include <memory>
+#include <SFML/Network.hpp>
+
+namespace NS
+{
+	class Networking
+	{
+	public:
+
+		static Networking* Get();
+
+		Networking(const Networking&) = delete;
+		Networking(Networking&&) = delete;
+		Networking& operator=(const Networking&) = delete;
+		Networking& operator=(Networking&&) = delete;
+
+	public:
+
+		void Connect(const sf::IpAddress& ServerAddress, const uint16_t ServerPort);
+
+	private:
+
+		Networking() = default;
+
+	private:
+
+		static std::unique_ptr<Networking> Instance_;
+		sf::TcpSocket MainSocket_;
+	};
+}
