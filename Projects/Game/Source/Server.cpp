a29715fac8280edfd84d1c94718083ca84ea11a9
HappySapeta <35849508+HappySapeta@users.noreply.github.com>
2025-11-28 17:42:34 +0000

    Revert "removed test code"
    This reverts commit da2e73e5371d682d9f4df892d0c6647467d15888.

--- Projects/Game/Source/Server.cpp
+++ Projects/Game/Source/Server.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 
 #include "GameConfiguration.h"
+#include "Tank.h"
 #include "Engine/Engine.h"
 
 using HRClock = std::chrono::high_resolution_clock;
@@ -9,12 +10,14 @@ using TimePoint = std::chrono::high_resolution_clock::time_point;
 using Duration = std::chrono::duration<float>;
 
 static float DeltaTimeSecs = 0.016f;
-constexpr float Radius = 100.0f;
 
 int main()
 {
 	NS::Engine* Engine = NS::Engine::Get();
 	Engine->StartSubsystems();
+	
+	NS::Tank* Tank = Engine->CreateActor<NS::Tank>();
+	Tank->SetPosition({100.0f, 100.0f});
 	
 	while (true)
 	{
